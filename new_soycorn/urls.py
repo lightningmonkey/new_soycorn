ed7def54b0865a55ab6df36c666f580c83172aab
Brian Cordonnier <bcordonn@gmail.com>
2016-04-24 21:33:24 -0500

    get bootstrap working ish

--- new_soycorn/urls.py
+++ new_soycorn/urls.py
@@ -5,10 +5,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', views.index, name='index'),
-    url(r'^article/(\d+)$', views.article_single, name='article_single'),
-    url(r'^article/page/(\d+)$', views.article_page, name='article_page'),
+                       url(r'^$', views.index, name='index'),
+                       url(r'^article/(\d+)$', views.article_single, name='article_single'),
+                       url(r'^article/page/(\d+)$', views.article_page, name='article_page'),
+                       url(r'^faq$', views.faq, name='faq'),
+                       url(r'^contact-us$', views.contact_us, name='contact_us'),
 
-    url(r'^admin/', include(admin.site.urls)),
+                       url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- new_soycorn/views.py
+++ new_soycorn/views.py
@@ -1,17 +1,24 @@
 import os
 from django.shortcuts import render, get_object_or_404
 from new_soycorn.models import Article
-from django.templatetags.static import static
 from django.conf import settings
 
 PAGE_LIMIT = 20
-PAGE_OFFSET = 20
+PAGE_OFFSET = PAGE_LIMIT
 
 
 def index(request):
     article_list = _get_articles(1)
     context = {'latest_article_list': article_list}
     return render(request, 'new_soycorn/index.html', context)
+
+
+def faq(request):
+    return render(request, 'new_soycorn/faq.html', {})
+
+
+def contact_us(request):
+    return render(request, 'new_soycorn/contact_us.html', {})
 
 
 def article_page(request, page_number):
@@ -39,10 +46,14 @@ def _get_articles(page_number):
 def article_single(request, article_id):
     article_obj = get_object_or_404(Article, pk=article_id)
     with open(os.path.join(settings.STATIC_ROOT, "articles/{0}".format(article_obj.file))) as f:
-        lines = f.read()
-    print("lines: {0}".format(lines))
-
+        all_lines = f.readlines()
+    # print("lines: {0}".format(lines))
+    date = all_lines[0]
+    title = all_lines[1]
+    lines = " ".join(all_lines[2:])
     context = {'article_str': article_obj.__str__(),
+               'date': date,
+               'title': title,
                'lines': lines}
 
     return render(request, 'new_soycorn/article_single.html', context)
